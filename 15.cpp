365d89a4ffc0add32607e3288b00f9cb852c68b1
Dasha <kuzdashad@gmail.com>
2024-12-11 14:07:12 +0500

    основной код

---
+++ 15.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <iomanip>
+#include <conio.h>
+#include "header.h"
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(static_cast<int>(time(NULL)));
+    create_right_map(); // создание правильной карты
+    do
+    {
+        create_game_map();
+    } while (check_map());
+    do // игровой цикл
+    {
+        screen();
+        get_direction();
+    } while (!check_map());
+    cout << "\nВы выиграли!\nИгра окончена!\n";
+    _getch();
+    return 0;
+}
